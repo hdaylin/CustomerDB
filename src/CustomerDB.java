e54a67c717b62d3a048214e09dcebf2738530579
hdaylin <hdaylin@yahoo.com>
2017-06-13 11:53:22 -0400

    done with error

--- src/CustomerDB.java
+++ src/CustomerDB.java
@@ -12,16 +12,17 @@ public class CustomerDB {
 
 		Scanner keyboard = new Scanner(System.in);
 
-		System.out.println("Press (1) to search for another customer or press (2) to Edit a customer's address. ");
+		System.out.println("Press (1) to search for another customer or press (2) to Edit a customer's address. Press anything else to quit. ");
 		int response = keyboard.nextInt();
+		keyboard.nextLine();
 		System.out.println("what is the person's last name? "); 
 
-		String name = keyboard.next();  
+		String name = keyboard.nextLine();  
 
-		int cont = 1;
+		//int cont = 1;
 
 
-		while (cont ==1 || cont ==2){ 
+		while (response ==1 || response ==2){ 
 			
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
@@ -52,11 +53,59 @@ public class CustomerDB {
 
 			case(2):
 
-			System.out.println("Enter a customer ID number"); 
-			cont = keyboard.nextInt(); 
-			break;  
-
-			} 
+			System.out.println("Enter the person's title.");
+			String title = keyboard.nextLine();
+			
+			System.out.println("Enter the person first name.");
+			String fname = keyboard.next(); 
+			
+			
+			System.out.println("Enter the person's street address"); 
+			String address = keyboard.nextLine(); 
+			
+			keyboard.nextLine();
+			
+			System.out.println("Enter the persons city."); 
+			String city = keyboard.next();  
+			
+			System.out.println("Enter the state");
+			String state = keyboard.next(); 
+			
+			System.out.println("Enter the persons zipcode."); 
+			String zip = keyboard.next(); 
+			
+			System.out.println("Enter the persons email address.");
+			String email = keyboard.next(); 
+			
+			System.out.println("Enter the persons position."); 
+			String position = keyboard.next(); 
+			
+			System.out.println("Enter the persons company.");
+			String company = keyboard.next();  
+			
+			String fullname = fname.concat(" ").concat(name);
+			
+			query = ("Insert INTO customers (FullName, Title, FirstName, LastName, StreetAddress, City, State, ZipCode, EmailAddress, Position, Company)"
+					+ " VALUES (?,?,?,?,?,?,?,?,?,?,?)");
+			preparedStmt = con.prepareStatement(query);
+			
+			
+			preparedStmt.setString(1, fullname);
+			preparedStmt.setString(2, title); 
+			preparedStmt.setString(3, fname);
+			preparedStmt.setString(4, name);
+			preparedStmt.setString(5, address); 
+			preparedStmt.setString(6, state);
+			preparedStmt.setString(7, city);
+			preparedStmt.setString(8, zip);
+			preparedStmt.setString(9, email);
+			preparedStmt.setString(10, position);
+			preparedStmt.setString(11, company);			
+			
+			preparedStmt.executeUpdate();
+			System.out.println("Inserted ");
+			break;
+			}
 			
 			con.close(); 
 
@@ -65,11 +114,12 @@ public class CustomerDB {
 		
 		} 
 		System.out.println("Press (1) to search for another customer or press (2) to Edit a customer's address.");
-		cont = keyboard.nextInt(); 
+		response = keyboard.nextInt(); 
+		keyboard.nextLine();
 
-		if(cont ==1){
+		if(response ==1){
 		System.out.println("what is the person's last name? "); 
-		name = keyboard.next();  
+		name = keyboard.nextLine();  
 		}
 
 		}  
